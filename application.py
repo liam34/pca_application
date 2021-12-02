974a6f19087559f3b57758708f0e3ecbf8eb0eab
liam34 <indemandcoding@gmail.com>
2021-12-01 22:16:40 -0700

    add test.ipynd and application_functions.py

--- application.py
+++ application.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from sklearn.decomposition import PCA
-from sklearn import preprocessing
+
+
+st.set_page_config(layout="wide")
 
 scatter_column, settings_column = st.beta_columns((4, 1))
+
+scatter_column.title("Multi-Dimensional Analysis")
+
+settings_column.title("Settings")
